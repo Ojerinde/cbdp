f29b1e576014f9d17a7f387644fd58aed1b46d6f
Joel Ojerinde <joelojerinde@gmail.com>
2023-03-02 15:12:17 +0100

    fix: fix conflict

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,29 +5,29 @@ import "./Header.css";
 const Header = () => {
   return (
     <>
-      <header class="main-header">
+      <header className="main-header">
         <Navigation />
-        <main class="main">
-          <div class="main-box">
-            <div class="main-content-box">
-              <h1 class="main-heading">
+        <main className="main">
+          <div className="main-box">
+            <div className="main-content-box">
+              <h1 className="main-heading">
                 Build Skills With Experts Any Time Anywhere
               </h1>
-              <p class="main-description">
+              <p className="main-description">
                 CBDP coding learning space is where students come to get most if
                 what is being taught in class and also to have a basic knowledge
                 of most programming Language to become more efficient and
                 proficient.
               </p>
 
-              <a href="/" class="course-link">
+              <a href="/" className="course-link">
                 Start Learning
               </a>
             </div>
 
-            <div class="illustration-box">
+            <div className="illustration-box">
               <img
-                class="illustration-img"
+                className="illustration-img"
                 src={headerImg}
                 alt="illustration of a boy with the computer"
               />
